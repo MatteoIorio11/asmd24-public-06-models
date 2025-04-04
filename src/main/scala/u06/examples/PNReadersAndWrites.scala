55ac083ec4de9443076160f9c044f9fcfd3e45ba
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-04 12:08:21 +0200

    fix: rw rules

--- src/main/scala/u06/examples/PNReadersAndWrites.scala
+++ src/main/scala/u06/examples/PNReadersAndWrites.scala
@@ -1,6 +1,7 @@
-package scala.u06.examples
+package u06.examples
 
 import u06.modelling.PetriNet
+
 
 object PNReadersAndWrites:
   enum Place:
@@ -12,16 +13,18 @@ object PNReadersAndWrites:
   export u06.utils.MSet
 
   def readersAndWriters = PetriNet[Place](
-    MSet(P1) ~~> MSet(P2), // P1 -> P2
-    MSet(P2) ~~> MSet(P3), // P2 -> P3
-    MSet(P2) ~~> MSet(P4), // P2 -> P4
-    MSet(P3) ~~> MSet(P5), // P3 -> P5
-    MSet(P4) ~~> MSet(P5), // P4 -> P5
-    MSet(P5) ~~> MSet(P6), // P5 -> P6 [Lock]
-    MSet(P5) ~~> MSet(P7), // P5 -> P7 [Lock]
-    MSet(P6) ~~> MSet(P1), // P6 -> P1
-    MSet(P7) ~~> MSet(P1),  // P7 -> P1
-    MSet(P7) ~~> MSet(P5) // P7 -> P5
+    MSet(P1) ~~> MSet(P2), // t1
+    MSet(P2) ~~> MSet(P3), // t2
+    MSet(P2) ~~> MSet(P4), // t3
+    MSet(P3) ~~> MSet(P5), // t4
+    MSet(P3) ~~> MSet(P6), // t4
+    MSet(P4) ~~> MSet(P7), // t5
+    MSet(P5) ~~> MSet(P3), // t4 [lock]
+    MSet(P5) ~~> MSet(P7), // t5 [Lock]
+    MSet(P6) ~~> MSet(P1), // t6
+    MSet(P6) ~~> MSet(P7) ^^^ MSet(P6), // t5
+    MSet(P7) ~~> MSet(P1),  // t7
+    MSet(P7) ~~> MSet(P5) // t7
 
   ).toSystem
 
---
+++ src/test/scala/u06/modelling/TestReadersWriters.scala
@@ -0,0 +1,1 @@
+package scala.u06.modelling class TestReadersWriters
