7aa4c2e65d939116c4d8b54bb680c0dc8a160b8e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-11 17:58:24 +0200

    feat: count totalDone and count FailToDone

--- src/main/scala/u07/examples/StochasticChannel.scala
+++ src/main/scala/u07/examples/StochasticChannel.scala
@@ -18,6 +18,19 @@ object StochasticChannel:
     Transition(DONE,1.0 --> DONE)
   )
 
+// 1) Compute the average time at which communication is done—across n runs
+// Compute the relative amount of time (0 % to 100 %) that the system is in fail state until communication is done—across n runs
+  def meanDone(trace: Seq[Trace[State]]): Double =
+    trace.filter(seq => seq.last.state == DONE).map(seq => seq.last.time).sum / trace.size
+
+  def meanFailToDone(trace: Seq[Trace[State]]): Double =
+    val totalFail = trace.count(seq => seq.contains(FAIL))
+    val failToDone = trace.count(seq => seq.contains(FAIL) && seq.last.state == DONE)
+    if totalFail > 0 then
+      failToDone / totalFail
+    else
+      0
+
 @main def mainStochasticChannel() =  // example run
   import StochasticChannel.*
   State.values.foreach(s => println(s"$s,${stocChannel.transitions(s)}"))
