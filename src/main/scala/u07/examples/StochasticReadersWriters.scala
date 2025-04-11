bbfac759bf0ac8e8b38a3c767ed86b589a6c815e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-11 12:12:41 +0200

    feat: add stochastic rw

--- src/main/scala/u07/examples/StochasticReadersWriters.scala
+++ src/main/scala/u07/examples/StochasticReadersWriters.scala
@@ -12,15 +12,13 @@ object StochasticReadersWriters extends App:
     case p1, p2, p3, p4, p5, p6, p7
 
   val stochasticRW = SPN[Place](
-    Trn(MSet(p1), m => 1.0,   MSet(p2),  MSet()),
-    Trn(MSet(p2), m => 200_000.0,   MSet(p3),  MSet()),
-    Trn(MSet(p2), m => 100_000.0,   MSet(p4),  MSet()),
-    Trn(MSet(p3), m => 100_000.0,   MSet(p5),  MSet()),
-    Trn(MSet(p3), m => 100_000.0,   MSet(p6),  MSet()),
-    Trn(MSet(p4), m => 100_000.0,   MSet(p5),  MSet()),
-    Trn(MSet(p4), m => 100_000.0,   MSet(p7),  MSet(p6)),
-    Trn(MSet(p6), m => m(p6) * 0.1,   MSet(p1),  MSet()),
-    Trn(MSet(p7), m => 0.2,   MSet(p1),  MSet()),
+    Trn(MSet(p1), m => 1.0,   MSet(p2),  MSet()), // t1
+    Trn(MSet(p2), m => 200_000.0,   MSet(p3),  MSet()), // t2
+    Trn(MSet(p2), m => 100_000.0,   MSet(p4),  MSet()), // t3
+    Trn(MSet(p3, p5), m => 100_000.0,   MSet(p6, p5),  MSet()), // t4
+    Trn(MSet(p4, p5), m => 100_000.0,   MSet(p7),  MSet(p6)), // t5
+    Trn(MSet(p6), m => m(p6) * 0.1,   MSet(p1),  MSet()), // t6
+    Trn(MSet(p7), m => 0.2,   MSet(p1, p5),  MSet()), // t7
   )
 
   export Place.*
@@ -28,6 +26,6 @@ object StochasticReadersWriters extends App:
   export u07.modelling.SPN.*
 
   println:
-    toCTMC(stochasticRW).newSimulationTrace(MSet(p1, p1), new Random)
+    toCTMC(stochasticRW).newSimulationTrace(MSet(p1, p5), new Random)
       .take(20)
       .toList.mkString("\n")
