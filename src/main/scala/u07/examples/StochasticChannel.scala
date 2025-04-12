931283432d7525fc62a7a9a72163b6e8dd225381
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-12 14:49:55 +0200

    feat: use builder for count fails and count fails to done

--- src/main/scala/u07/examples/StochasticChannel.scala
+++ src/main/scala/u07/examples/StochasticChannel.scala
@@ -2,6 +2,8 @@ package u07.examples
 
 import u07.modelling.CTMC
 
+import scala.u07.examples.ExtractorBuilder
+
 object StochasticChannel:
   enum State:
     case IDLE, SEND, DONE, FAIL;
@@ -20,12 +22,19 @@ object StochasticChannel:
 
 // 1) Compute the average time at which communication is done—across n runs
 // Compute the relative amount of time (0 % to 100 %) that the system is in fail state until communication is done—across n runs
+
+  def tracesApi[X](trace: Seq[Trace[State]], function: Seq[Trace[State]] => X): X =
+    function(trace)
+
   def meanDone(trace: Seq[Trace[State]]): Double =
-    trace.filter(seq => seq.last.state == DONE).map(seq => seq.last.time).sum / trace.size
+    ExtractorBuilder(trace)
+      .applyPredicate(trace => trace.last == DONE)
+      .applyFunctionAndOperator(trace => trace.last.time, (i1, i2) => i1 + i2)
+//    tracesApi(trace, f=> trace.filter(seq => seq.last.state == DONE).map(seq => seq.last.time).sum / trace.size)
 
   def meanFailToDone(trace: Seq[Trace[State]]): Double =
-    val totalFail = trace.count(seq => seq.contains(FAIL))
-    val failToDone = trace.count(seq => seq.contains(FAIL) && seq.last.state == DONE)
+    val totalFail = ExtractorBuilder(trace).applyCount(trace => trace.contains(FAIL))
+    val failToDone = ExtractorBuilder(trace).applyCount(trace => trace.contains(FAIL) && trace.last.state == DONE)
     if totalFail > 0 then
       failToDone / totalFail
     else
