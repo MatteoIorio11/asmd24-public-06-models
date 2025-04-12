71f057e59a4819882efb367c0856746967d7bc54
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-12 16:27:16 +0200

    fix: add new version of rw

--- src/main/scala/u06/examples/PNReadersAndWrites.scala
+++ src/main/scala/u06/examples/PNReadersAndWrites.scala
@@ -23,21 +23,18 @@ object PNReadersAndWrites:
   ).toSystem
 
   def readersAndWritesWithPriority = PetriNet[Place](
-    MSet(P1) ~~> MSet(P2),                  // t1: Process starts
-    MSet(P2) ~~> MSet(P3),                  // t2: Process decides to read
-    MSet(P2) ~~> MSet(P4),                  // t3: Process decides to write
-    MSet(P3) ~~> MSet(P5),                  // t4: Start reading
-    MSet(P3) ~~> MSet(P8),                  // NEW: Reader goes to waiting queue if can't read immediately
-    MSet(P8) ~~> MSet(P5),                  // NEW: Reader from queue starts reading (priority)
-    MSet(P5) ~~> MSet(P6),                  // t5: Finish reading
-    MSet(P4) ~~> MSet(P7),                  // t6: Start writing (only if no readers waiting)
-    MSet(P6) ~~> MSet(P1),                  // t7: Reader goes back to idle
-    MSet(P7) ~~> MSet(P1),                  // t8: Writer goes back to idle
-    MSet(P7) ~~> MSet(P8) ^^^ MSet(P7)      // NEW: Block writers if readers are waiting
+    MSet(P1) ~~> MSet(P2),                   // t1: request
+    MSet(P2) ~~> MSet(P3),                   // t2: reader request
+    MSet(P2) ~~> MSet(P4),                   // t3: writer request
+    MSet(P3, P5) ~~> MSet(P5, P6),           // t4: reader enter
+    MSet(P4, P5)  ~~> MSet(P7) ^^^ MSet(P3),  // t5: writer enter (guarded by "no readers")
+    MSet(P6) ~~> MSet(P1),                   // t6: reader done
+    MSet(P7) ~~> MSet(P1, P5),               // t7: writer done
   ).toSystem
 
 
 @main def mainReadersAndWriters =
   import PNReadersAndWrites.*
-  println(readersAndWriters.paths(MSet(P1, P5), 7).toList.mkString("\n"))
-//  println(readersAndWritesWithPriority.paths(MSet(P1), 7).toList.mkString("\n"))
+//  println(readersAndWriters.paths(MSet(P1, P5), 7).toList.mkString("\n"))
+  println(readersAndWritesWithPriority.paths(MSet(P3, P4, P5), 10).toList.mkString("\n"))
+  // List({P5|P1|P1}, {P2|P5|P1}, {P5|P1|P3}, {P2|P5|P3}, {P2|P5|P6}, {P2|P5|P1}, {P2|P2|P5}, {P2|P5|P4}, {P5|P4|P3}, {P5|P6|P4})
