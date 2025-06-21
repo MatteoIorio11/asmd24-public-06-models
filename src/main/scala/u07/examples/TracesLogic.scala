9e2f208873f5bfb19816fb2410cdaddae497ad68
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-21 16:12:23 +0200

    refactor: change Class name, add new operation for mean in a State

--- src/main/scala/u07/examples/TracesLogic.scala
+++ src/main/scala/u07/examples/TracesLogic.scala
@@ -3,10 +3,10 @@ package scala.u07.examples
 import u07.examples.StochasticChannel.State
 import u07.modelling.CTMCSimulation.Trace
 
-class ExtractorBuilder[X](private val traces: Seq[X]):
+class TracesLogic[X](private val traces: Seq[X]):
   private var buffer = List().appendedAll(traces)
 
-  def applyPredicate(predicate: X => Boolean): ExtractorBuilder[X] =
+  def applyPredicate(predicate: X => Boolean): TracesLogic[X] =
     buffer = buffer.filter(trace => predicate(trace))
     this
 
@@ -15,5 +15,11 @@ class ExtractorBuilder[X](private val traces: Seq[X]):
 
   def applyFunctionAndOperator[Y](function: X => Y, operator: (Y, Y) => Y): Y =
     buffer.map(trace => function(trace)).reduce((i1, i2) => operator(i1, i2))
+
+  def meanForState(predicate: X => Boolean): Double =
+    val totalInState: Double = this.buffer.count(predicate)
+    buffer.size match
+      case 0 => 0
+      case x => totalInState / x
 
   def collection: Seq[X] = buffer
