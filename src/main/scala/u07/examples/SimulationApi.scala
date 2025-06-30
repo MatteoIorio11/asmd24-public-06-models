9194467f2b04898df75bdbe17ac133d6fc8e54c5
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-30 17:14:48 +0200

    feat: add implementation for every method

--- src/main/scala/u07/examples/SimulationApi.scala
+++ src/main/scala/u07/examples/SimulationApi.scala
@@ -6,11 +6,40 @@ object SimulationApi:
   private type Simulations[S] = Seq[Trace[S]]
   trait SimulationBuffer[S]:
     def simulations: Simulations[S]
-  trait SimulationOperations[S]:
+    def size: Int
+  trait SimulationOperation[S]:
     def applyFunction[X](function: Trace[S] => Trace[X]): SimulationBuffer[X]
-    def applyOperator[X](operator: (Trace[S], Trace[S] => Trace[X])): SimulationBuffer[X]
-    def takeFirstN(n: Int): SimulationBuffer[S]
+    def applyTakeFirstN(n: Int): SimulationBuffer[S]
+  trait SimulationFilter[S]:
+    def applyFilter(predicate: Trace[S] => Boolean): SimulationBuffer[S]
+    def applyTakeUntil(predicate: Trace[S] => Boolean): SimulationBuffer[S]
+    def applyCount(predicate: Trace[S] => Boolean): Int
+  trait SimulationStatistics[S]:
+    def meanForTrace(predicate: Trace[S] => Boolean): Double
 
-  case class SimulationImpl[S](override val simulations: Simulations[S]) extends SimulationBuffer[S] with SimulationOperations[S]:
+  case class SimulationImpl[S](override val simulations: Simulations[S]) extends SimulationBuffer[S]
+    with SimulationOperation[S]
+    with SimulationFilter[S]
+    with SimulationStatistics[S]:
 
+    override def size: Int = simulations.size
 
+    override def applyFunction[X](function: Trace[S] => Trace[X]): SimulationBuffer[X] =
+      SimulationImpl(simulations map function)
+
+    override def applyTakeFirstN(n: Int): SimulationBuffer[S] =
+      SimulationImpl(simulations take (n))
+
+    override def applyFilter(predicate: Trace[S] => Boolean): SimulationBuffer[S] =
+      SimulationImpl(simulations filter predicate)
+
+    override def applyCount(predicate: Trace[S] => Boolean): Int =
+      simulations count predicate
+
+    override def meanForTrace(predicate: Trace[S] => Boolean): Double = size match
+      case 0 => 0
+      case x =>
+        applyCount(predicate) / x.toDouble
+
+    override def applyTakeUntil(predicate: Trace[S] => Boolean): SimulationBuffer[S] =
+      SimulationImpl(simulations takeWhile predicate)
