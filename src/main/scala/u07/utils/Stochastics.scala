5044e3b0b57636ecf05e97ab646fa32df6078f45
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-02 12:28:35 +0200

    feat: add Random Unit Tester task. Use RandomGenerator instead of scala random class

--- src/main/scala/u07/utils/Stochastics.scala
+++ src/main/scala/u07/utils/Stochastics.scala
@@ -1,10 +1,16 @@
 package u07.utils
 
-import scala.util.Random
+import java.util.Random
+import java.util.random.RandomGenerator
 
 object Stochastics:
 
-  given Random = new Random()
+  /**
+   * <<RANDOM-UNIT-TESTER>>
+   * How do we unit-test with randomness? And how we test at all with randomness? Think about this in general. Try to create a
+   * repeatable unit test for Statistics as in utils.StochasticSpec.
+   */
+  given RandomGenerator = new Random()
 
   // (p1,a1),...,(pn,an) --> (p1,a1),(p1+p2,a2),..,(p1+..+pn,an)
   def cumulative[A](l: List[(Double, A)]): List[(Double, A)] =
@@ -12,7 +18,7 @@ object Stochastics:
       case ((r, _), (r2, a2)) => (r + r2, a2)
 
   // (p1,a1),...,(pn,an) --> ai, selected randomly and fairly
-  def draw[A](cumulativeList: List[(Double,A)])(using rnd: Random): A =
+  def draw[A](cumulativeList: List[(Double,A)])(using rnd: RandomGenerator): A =
     val rndVal = rnd.nextDouble() * cumulativeList.last._1
     cumulativeList.collectFirst:
       case (r, a) if r >= rndVal => a
@@ -20,7 +26,7 @@ object Stochastics:
 
   // (p1,a1),...,(pn,an) --> {a1 -> P1%,...,an -> Pn%}
   def statistics[A](choices: Set[(Double,A)], size: Int)
-                   (using rnd: Random): Map[A, Int] =
+                   (using rnd: RandomGenerator): Map[A, Int] =
     (1 to size).map(_ => draw(cumulative(choices.toList)))
                 .groupBy(identity).view.mapValues(_.size).toMap
 
