fe70310469a834a73d0349ee1351bf0ddb431e8e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-02 12:29:04 +0200

    refactor(test): use mockito for mockito the RandomGenerator. Write new tests

--- src/test/scala/u07/utils/StochasticsSpec.scala
+++ src/test/scala/u07/utils/StochasticsSpec.scala
@@ -1,21 +1,39 @@
 package u07.utils
 
+import org.mockito.Mockito
+import org.mockito.Mockito.when
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.*
 import org.scalactic.Tolerance.convertNumericToPlusOrMinusWrapper
+import org.scalatest.BeforeAndAfter
 
-class StochasticsSpec extends AnyFunSuite:
-  import Stochastics.given
+import java.util.Random
+import java.util.random.RandomGenerator
 
+/**
+ * <<RANDOM-UNIT-TESTER>>
+ * How do we unit-test with randomness? And how we test at all with randomness? Think about this in general. Try to create a
+ * repeatable unit test for Statistics as in utils.StochasticSpec.
+ */
+class StochasticsSpec extends AnyFunSuite with BeforeAndAfter:
+  val myMockRandom: RandomGenerator = Mockito.mock(classOf[RandomGenerator])
   val choices = Set( 1.0->"a", 2.0->"b", 3.0->"c")
+
+  before:
+    when(myMockRandom.nextDouble()).thenReturn(0.3)
 
   test("Choices should correctly give cumulative list"):
       Stochastics.cumulative(choices.toList) shouldBe
         List((1.0,"a"), (3.0,"b"), (6.0,"c"))
 
   test("Choices should correctly draw"):
-    val map = Stochastics.statistics(choices, 10000)
+    when(myMockRandom.nextDouble()).thenReturn(0.05)
+    var map = Stochastics.statistics(choices, 10000)(using myMockRandom)
+    map shouldBe Map("a" -> 10_000)
+    when(myMockRandom.nextDouble()).thenReturn(0.3)
+    map = Stochastics.statistics(choices, 10000)(using myMockRandom)
+    map shouldBe Map("b" -> 10_000)
+    when(myMockRandom.nextDouble()).thenReturn(0.6)
+    map = Stochastics.statistics(choices, 10000)(using myMockRandom)
+    map shouldBe Map("c" -> 10_000)
 
-    map("a") shouldBe 1666 +- 500
-    map("b") shouldBe 3333 +- 500
-    map("c") shouldBe 5000 +- 500
