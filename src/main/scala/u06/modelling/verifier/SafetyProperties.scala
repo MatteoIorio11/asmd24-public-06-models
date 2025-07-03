3a4c3e11482b3b071246b620a55a69326bc8db35
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-03 13:52:25 +0200

    feat: add rw mutual exclusion and bounded safety check

--- src/main/scala/u06/modelling/verifier/SafetyProperties.scala
+++ src/main/scala/u06/modelling/verifier/SafetyProperties.scala
@@ -8,6 +8,17 @@ object SafetyProperties:
 
   trait MultiSetSafety[T] extends Safety[MSet[T]]
 
-  case class MutualExclusion[T](currentState: T) extends MultiSetSafety[T]:
+  //Mutual exclusion bounded to one toke.
+  case class MutualExclusion[T](private val currentState: T) extends MultiSetSafety[T]:
     override def isSafe(s: MSet[T]): Boolean = s(currentState) <= 1
 
+  //It is not possible to have readers and writers at the same time.
+  case class RWMutualExclusion[T](private val readers: Set[T], writers: Set[T]) extends MultiSetSafety[T]:
+    override def isSafe(s: MSet[T]): Boolean =
+      val readersCount = readers.map(r => s(r)).sum
+      val writersCount = writers.map(w => s(w)).sum
+      !((readersCount > 0 && writersCount > 0) && writersCount >= 1)
+
+  //A place has at most maxTokens tokens.
+  case class Bounded[T](private val state: T, private val maxTokens: Int) extends MultiSetSafety[T]:
+    override def isSafe(s: MSet[T]): Boolean = s(state) <= maxTokens
