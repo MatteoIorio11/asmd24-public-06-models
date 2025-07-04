9bc2378c0b0696148be61d915c830c6d9d90594d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-04 12:25:18 +0200

    doc: add scala doc

--- src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
+++ src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
@@ -3,26 +3,30 @@ package scala.u06.modelling.verifier
 import scala.u06.utils.MSet
 
 object BehaviourProperties:
+  /**
+   * Safety property of a petri net
+   */
   trait Property[T]:
     def isValid(traces: Seq[T]): Boolean
 
-  trait MultiSetProperty[T] extends Property[MSet[T]]
+  private trait MultiSetProperty[T] extends Property[MSet[T]]
 
-
-  // Reachability:
-  // definition: Whether a certain marking (state) can be reached from the initial marking.
+  /** Reachability
+   * Whether a certain marking (state) can be reached from the initial marking.
+   */
   private case class Reachability[T](private val state: T) extends MultiSetProperty[T]:
     override def isValid(traces: Seq[MSet[T]]): Boolean = traces.exists(multiSet => multiSet(state) > 0)
 
-
-  // Deadlock-freeness
-  // Definition: Checks that the system never reaches a marking where no transition is enabled (no further progress possible).
+  /** Deadlock-freeness
+   * Definition: Checks that the system never reaches a marking where no transition is enabled (no further progress possible).
+   */
   private case class DeadlockFreeness[T](private val deadlockCondition: T => Iterable[T]) extends Property[T]:
     override def isValid(traces: Seq[T]): Boolean =
       !(traces exists (s => deadlockCondition(s).isEmpty))
 
-  // Fairness
-  // Definition: Ensures that no transition is indefinitely postponed if it is enabled infinitely often.
+  /** Fairness
+   * Definition: Ensures that no transition is indefinitely postponed if it is enabled infinitely often.
+   */
   private case class Fairness[T](private val allowedStates: Set[T]) extends MultiSetProperty[T]:
     override def isValid(traces: Seq[MSet[T]]): Boolean =
       allowedStates.forall(state => traces.map(mset => mset(state)).sum > 0)
