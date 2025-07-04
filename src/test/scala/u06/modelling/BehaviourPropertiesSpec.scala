67ee6b015a453c6687ddbde8ce932f53a51d4b25
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-04 14:15:46 +0200

    refactor: imports

--- src/test/scala/u06/modelling/BehaviourPropertiesSpec.scala
+++ src/test/scala/u06/modelling/BehaviourPropertiesSpec.scala
@@ -3,15 +3,10 @@ package scala.u06.modelling
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.shouldBe
 
-import scala.u06.examples.PNReadersAndWrites
-import scala.u06.examples.PNReadersAndWrites.{P1, P5, P6, P7, Place, readersAndWriters}
+import scala.u06.examples.PNReadersAndWrites.*
 import scala.u06.modelling.SystemAnalysis.behaviourProperty
 import scala.u06.modelling.verifier.BehaviourProperties.*
-import scala.u06.modelling.verifier.BehaviourProperties.{Reachability, reachabilty}
-import scala.u06.modelling.verifier.SafetyProperties.{Bounded, RWMutualExclusion, bounded, mutualExclusion, rwMutualExclusion}
-import scala.u06.modelling.verifier.BehaviourProperties.*
-import scala.u06.modelling.System.*
-import scala.u06.examples.PNReadersAndWrites.*
+import scala.u06.modelling.verifier.SafetyProperties.*
 
 
 class BehaviourPropertiesSpec extends AnyFunSuite:
