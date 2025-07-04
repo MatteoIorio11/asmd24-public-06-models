cced87decdaca6750c0acfbda4fd8eb75bfc8a38
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-04 12:33:25 +0200

    doc: add scala doc

--- src/main/scala/u06/modelling/verifier/SafetyProperties.scala
+++ src/main/scala/u06/modelling/verifier/SafetyProperties.scala
@@ -3,23 +3,32 @@ package scala.u06.modelling.verifier
 import scala.u06.utils.MSet
 
 object SafetyProperties:
+  /**
+   * Safety property
+   */
   trait Safety[S]:
     def isSafe(s: S): Boolean
 
   trait MultiSetSafety[T] extends Safety[MSet[T]]
 
-  //Mutual exclusion bounded to one toke.
+  /** Mutual Exclusion
+   * Place bounded to one take.
+   */
   case class MutualExclusion[T](private val currentState: T) extends MultiSetSafety[T]:
     override def isSafe(s: MSet[T]): Boolean = s(currentState) <= 1
 
-  //It is not possible to have readers and writers at the same time.
+  /** Readers and Writers Mutual Exclusion
+   * It is not possible to have readers and writers at the same time.
+   */
   case class RWMutualExclusion[T](private val positions: Map[T, Set[T]]) extends MultiSetSafety[T]:
     override def isSafe(s: MSet[T]): Boolean =
       val first = positions.keys.map(k => s(k)).sum
       val second = positions.values.flatten.map(w => s(w)).sum
       !((first > 0 && second > 0) && second >= 1)
 
-  //A place has at most maxTokens tokens.
+  /** Bounded
+   * A place has at most maxTokens tokens.
+   */
   case class Bounded[T](private val state: T, private val maxTokens: Int) extends MultiSetSafety[T]:
     override def isSafe(s: MSet[T]): Boolean = s(state) <= maxTokens
 
