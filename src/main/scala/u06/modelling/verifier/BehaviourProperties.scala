067d0d0bac4ff125e842c8828ac565ae46ed11a1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-04 11:59:23 +0200

    fix: work on state and not MSet

--- src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
+++ src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
@@ -15,9 +15,9 @@ object BehaviourProperties:
 
   // Deadlock-freeness
   // Definition: Checks that the system never reaches a marking where no transition is enabled (no further progress possible).
-  case class DeadlockFreeness[T](private val deadlockCondition: T => Iterable[T]) extends Property[MSet[T]]:
-    override def isValid(traces: Seq[MSet[T]]): Boolean =
-      !(traces flatten (trace => trace.asList) exists (s => deadlockCondition(s).isEmpty))
+  case class DeadlockFreeness[T](private val deadlockCondition: T => Iterable[T]) extends Property[T]:
+    override def isValid(traces: Seq[T]): Boolean =
+      !(traces exists (s => deadlockCondition(s).isEmpty))
 
   // Fairness
   // Definition: Ensures that no transition is indefinitely postponed if it is enabled infinitely often.
@@ -27,5 +27,5 @@ object BehaviourProperties:
 
 
   def reachabilty[T](state: T): Property[MSet[T]] = Reachability[T](state)
-  def deadlockFreeness[T](deadlockCondition: T => Iterable[T]): Property[MSet[T]] = DeadlockFreeness[T](deadlockCondition)
+  def deadlockFreeness[T](deadlockCondition: T => Iterable[T]): Property[T] = DeadlockFreeness[T](deadlockCondition)
   def fairness[T](allowedStates: Set[T]): Property[MSet[T]] = Fairness[T](allowedStates)
