95811def5cd665d1e6f886bdae68d5395d068edd
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-04 12:15:31 +0200

    feat: add Reveribility logic

--- src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
+++ src/main/scala/u06/modelling/verifier/BehaviourProperties.scala
@@ -6,26 +6,35 @@ object BehaviourProperties:
   trait Property[T]:
     def isValid(traces: Seq[T]): Boolean
 
+  trait MultiSetProperty[T] extends Property[MSet[T]]
+
 
   // Reachability:
   // definition: Whether a certain marking (state) can be reached from the initial marking.
-  case class Reachability[T](private val state: T) extends Property[MSet[T]]:
+  private case class Reachability[T](private val state: T) extends MultiSetProperty[T]:
     override def isValid(traces: Seq[MSet[T]]): Boolean = traces.exists(multiSet => multiSet(state) > 0)
 
 
   // Deadlock-freeness
   // Definition: Checks that the system never reaches a marking where no transition is enabled (no further progress possible).
-  case class DeadlockFreeness[T](private val deadlockCondition: T => Iterable[T]) extends Property[T]:
+  private case class DeadlockFreeness[T](private val deadlockCondition: T => Iterable[T]) extends Property[T]:
     override def isValid(traces: Seq[T]): Boolean =
       !(traces exists (s => deadlockCondition(s).isEmpty))
 
   // Fairness
   // Definition: Ensures that no transition is indefinitely postponed if it is enabled infinitely often.
-  case class Fairness[T](private val allowedStates: Set[T]) extends Property[MSet[T]]:
+  private case class Fairness[T](private val allowedStates: Set[T]) extends MultiSetProperty[T]:
     override def isValid(traces: Seq[MSet[T]]): Boolean =
       allowedStates.forall(state => traces.map(mset => mset(state)).sum > 0)
 
+  /** Reversibility
+   * Whether the system can always return to the initial marking, no matter what marking is reached
+   */
+  private case class Reversibility[T](private val initialState: T) extends MultiSetProperty[T]:
+    override def isValid(traces: Seq[MSet[T]]): Boolean =
+      (traces map (mset => mset(initialState))).sum > 1
 
   def reachabilty[T](state: T): Property[MSet[T]] = Reachability[T](state)
   def deadlockFreeness[T](deadlockCondition: T => Iterable[T]): Property[T] = DeadlockFreeness[T](deadlockCondition)
   def fairness[T](allowedStates: Set[T]): Property[MSet[T]] = Fairness[T](allowedStates)
+  def reversibility[T](initialState: T): Property[MSet[T]] = Reversibility[T](initialState)
